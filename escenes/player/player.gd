347a1175a24d97dda60129a0dbb9f1ddab27db08
c-dez <hdezcruzcarlos@gmail.com>
2025-07-05 14:44:56 -0600

    encapsulado player.gd

--- escenes/player/player.gd
+++ escenes/player/player.gd
@@ -6,17 +6,16 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+func _physics_process(_delta: float) -> void:
+	move()
+	jump()
+	gravity(_delta)
+	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	move_and_slide()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
+func move() -> void:
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -25,5 +24,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	pass
 
-	move_and_slide()
+
+func jump() -> void:
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+func gravity(_delta) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * _delta
