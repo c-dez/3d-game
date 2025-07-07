c10a1e5fe19dd3e281d9a73e21815d757706d92c
c-dez <hdezcruzcarlos@gmail.com>
2025-07-06 22:37:49 -0600

    terminado move()

--- escenes/player/camera/player_camera.gd
+++ escenes/player/camera/player_camera.gd
@@ -8,19 +8,17 @@ extends SpringArm3D
 var mouse_sens: float = 0.25 / 3 # 60 fps
 var camera_input_direction: Vector2 = Vector2.ZERO
 
-func _ready() -> void:
-	# top_level = true
-	# if player is not Player:
-	# 	printerr("PlayerCamera reference to Player is invalid")
 
+func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
+
 func _process(_delta: float) -> void:
-	var degres:int = 30
+	var degres: int = 30
 	# vertical
 	rotation.x -= camera_input_direction.y * _delta
 	#horizontal
-	rotation.y -= camera_input_direction.x * _delta 
+	rotation.y -= camera_input_direction.x * _delta
 	# x clamp(vertical)
 	rotation.x = clamp(rotation.x, deg_to_rad(-degres), deg_to_rad(degres))
 	#reset
@@ -30,4 +28,3 @@ func _process(_delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		camera_input_direction = event.screen_relative * mouse_sens
-	pass
--- escenes/player/player.gd
+++ escenes/player/player.gd
@@ -2,15 +2,14 @@ class_name Player
 extends CharacterBody3D
 
 
-const SPEED: float = 5.0
+const SPEED: float = 8.0
 const JUMP_VELOCITY: float = 4.5
 
-@export var player_camera : PlayerCamera
-@onready var visuals:MeshInstance3D = get_node("Visuals")
+@export var player_camera: PlayerCamera
+@onready var visuals: MeshInstance3D = get_node("Visuals")
 
 func _ready() -> void:
-	if player_camera is not PlayerCamera:
-		printerr("player_camara reference is invalid")
+	debug_start()
 
 func _physics_process(_delta: float) -> void:
 	move()
@@ -22,15 +21,24 @@ func _physics_process(_delta: float) -> void:
 
 
 func move() -> void:
-	var input_dir: Vector2 = Input.get_vector(Keybindings.left, Keybindings.right, Keybindings.forward, Keybindings.backwards)
-	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+	# movimiento normalizado combinando los inputs de jugador con la direccion de que la camera mira
+	var raw_input: Vector2 = Input.get_vector(Keybindings.left, Keybindings.right, Keybindings.forward, Keybindings.backwards)
+
+	# obtiene los vectores de la camara
+	var forward: Vector3 = player_camera.global_basis.z
+	var right: Vector3 = player_camera.global_basis.x
+	# ignorar  y de forward/right para mantener movimiento en z x constante
+	forward.y = 0
+	right.y = 0
+	# combina los valores de camera vars y raw_input para crear un Vector3 para que "enfrente" siempre sea hacia donde la camara mira
+	var direction: Vector3 = forward * raw_input.y + right * raw_input.x
+	direction = direction.normalized()
+	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	pass
 
 
 func jump() -> void:
@@ -43,4 +51,7 @@ func gravity(_delta: float) -> void:
 		velocity += get_gravity() * _delta
 
 
-# entonces helix?
+func debug_start() -> void:
+	# codigos para prevenir errores en _start
+	if player_camera is not PlayerCamera:
+		printerr("player_camara reference is invalid")
