8168d0b8e2875ab536e6ff7b644d3fc641da72f8
c-dez <hdezcruzcarlos@gmail.com>
2025-07-08 13:49:17 -0600

    empezando sistema para escalar escalera

--- scripts/keybinings/keybindings.gd
+++ scripts/keybinings/keybindings.gd
@@ -14,6 +14,13 @@ var jump: String = "jump"
 var jump_buffer_timer: Timer
 var buffer_active_time: float = 0.3
 
+# Actions
+var action: String = "action"
+
+#climbing
+var climb:String = "forward"
+var descend:String = "backwards"
+
 
 func _ready() -> void:
 	buffer_timers_settup()
@@ -33,3 +40,34 @@ func buffer_timers_settup() -> void:
 	jump_buffer_timer.one_shot = true
 	jump_buffer_timer.autostart = false
 	add_child(jump_buffer_timer)
+
+# un state machine da seguimiento a estado moverse, escalando escalera
+# dependiendo de el estado se asignan y desasignan keybindings
+
+func state_keys() -> void:
+	match MoveStateMachine.current_state:
+		MoveStateMachine.STATE.MOVING:
+			# clear_keys()
+			assign_moving_keys()
+		MoveStateMachine.STATE.CLIMBING:
+			clear_keys()
+			assign_climbing_keys()
+			
+		_:
+			pass
+
+func assign_moving_keys():
+	forward = "forward"
+	backwards = "backwards"
+	left = "left"
+	right = "right"
+
+func assign_climbing_keys():
+	climb = "forward"
+	descend = "backwards"
+
+func clear_keys():
+	forward = ""
+	backwards = ""
+	left = ""
+	right = ""
---
+++ scripts/move_state_machine/move_state_machine.gd
@@ -0,0 +1,14 @@
+# autoload moveStateMachine
+extends Node
+
+
+enum STATE {
+	MOVING,
+	CLIMBING,
+}
+
+var current_state:int = 0
+
+
+
+
